0e3264c00e4aa1f5f10204dcf22a828380625b4f
Voruarn0362 <66000419+Voruarn@users.noreply.github.com>
2023-09-02 10:05:17 +0800

    Add files via upload

---
+++ EORSSD_Train.py
@@ -0,0 +1,258 @@
+from tqdm import tqdm
+import utils
+import os
+import random
+import argparse
+import numpy as np
+import sys
+
+from torch.utils import data
+from datasets.EORSSD_Dataset import EORSSDDataset
+from utils import ext_transforms as et
+from metrics.SOD_metrics import SODMetrics
+from network.FPSNet import FPSNet
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+
+from torch.utils.tensorboard import SummaryWriter
+import pytorch_ssim
+import pytorch_iou
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--trainset_path", type=str, 
+        default='',
+        help="path to Dataset")
+    parser.add_argument("--testset_path", type=str, 
+        default='',
+        help="path to Dataset")
+    
+    parser.add_argument("--dataset", type=str, default='EORSSD', help='Name of dataset:[EORSSD, ORSSD]')
+    parser.add_argument("--num_classes", type=int, default=2,
+                        help='num_classes')
+    
+    parser.add_argument("--backbone", type=str, default='resnet50',
+        help='model name:[pvt_small, resnet50, hrnetv2_32]')
+    parser.add_argument("--model", type=str, default='FPSNet',
+        help='model name')
+
+    parser.add_argument("--epochs", type=int, default=60,
+                        help="epoch number (default: 60)")
+    parser.add_argument("--total_itrs", type=int, default=30e3,
+                        help="total_itrs")
+    parser.add_argument("--lr", type=float, default=0.001,
+                        help="learning rate (default: 0.001)")
+    parser.add_argument("--lr_policy", type=str, default='poly', choices=['poly', 'step'],
+                        help="learning rate scheduler policy")
+    parser.add_argument("--step_size", type=int, default=10)
+  
+    parser.add_argument("--batch_size", type=int, default=16,
+                        help='batch size ')
+    parser.add_argument("--trainsize", type=int, default=320)
+
+    parser.add_argument("--n_cpu", type=int, default=4,
+                        help="download datasets")
+    
+    parser.add_argument("--ckpt", type=str,
+            default=None, help="restore from checkpoint")
+    parser.add_argument("--loss_type", type=str, default='bsi', 
+                        help="loss type:[bce, bsi]")
+    parser.add_argument("--gpu_id", type=str, default='0', help="GPU ID")
+    parser.add_argument("--weight_decay", type=float, default=1e-4,
+                        help='weight decay (default: 1e-4)')
+    parser.add_argument("--random_seed", type=int, default=1,
+                        help="random seed (default: 1)")
+    parser.add_argument("--val_interval", type=int, default=5,
+                        help="epoch interval for eval (default: 100)")
+
+    return parser
+
+
+def get_dataset(opts):
+
+    train_dst = EORSSDDataset(is_train=True,voc_dir=opts.trainset_path, trainsize=opts.trainsize)
+    val_dst = EORSSDDataset(is_train=False,voc_dir=opts.testset_path, trainsize=opts.trainsize)
+    return train_dst, val_dst
+
+
+def validate(opts, model, loader, device,  metrics):
+    metrics.reset()
+    with torch.no_grad():
+        for step, (images, labels) in tqdm(enumerate(loader)):
+
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.float32)
+
+            SOUTS=model(images)
+              
+            outputs=SOUTS[0]
+            preds=outputs.squeeze()
+            labels=labels.squeeze()
+    
+            metrics.update(preds, labels)
+
+        score = metrics.get_results()
+    return score
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+bce_loss = nn.BCELoss(size_average=True)
+ssim_loss = pytorch_ssim.SSIM(window_size=11,size_average=True)
+iou_loss = pytorch_iou.IOU(size_average=True)
+
+def bce_ssim_loss(pred,target):
+	bce_out = bce_loss(pred,target)
+	ssim_out = 1 - ssim_loss(pred,target)
+	iou_out = iou_loss(pred,target)
+
+	loss = bce_out + ssim_out + iou_out
+
+	return loss
+
+def bsi_DS5(d0, d1, d2, d3, d4, labels_v):
+    loss0 = bce_ssim_loss(d0,labels_v)
+    loss1 = bce_ssim_loss(d1,labels_v)
+    loss2 = bce_ssim_loss(d2,labels_v)
+    loss3 = bce_ssim_loss(d3,labels_v)
+    loss4 = bce_ssim_loss(d4,labels_v)
+
+    loss = loss0 + loss1 + loss2/2 + loss3/4 + loss4/8 
+    return loss
+
+def bce_DS5(d0, d1, d2, d3, d4, labels_v):
+    loss0 = bce_loss(d0,labels_v)
+    loss1 = bce_loss(d1,labels_v)
+    loss2 = bce_loss(d1,labels_v)
+    loss3 = bce_loss(d1,labels_v)
+    loss4 = bce_loss(d1,labels_v)
+
+    loss = loss0 + loss1 + loss2/2 + loss3/4 + loss4/8 
+    return loss
+
+
+
+def main():
+    if not os.path.exists('CHKP'):
+        utils.mkdir('CHKP')
+
+    opts = get_argparser().parse_args()
+
+    tb_writer = SummaryWriter()
+    
+    torch.cuda.empty_cache()
+    os.environ['CUDA_VISIBLE_DEVICES'] = opts.gpu_id
+    
+    print("Device: %s" % device)
+
+    torch.manual_seed(opts.random_seed)
+    np.random.seed(opts.random_seed)
+    random.seed(opts.random_seed)
+
+    train_dst, val_dst = get_dataset(opts)
+    opts.total_itrs=opts.epochs * (len(train_dst) // opts.batch_size)
+    print('opts:',opts)
+
+    train_loader = data.DataLoader(
+        train_dst, batch_size=opts.batch_size, shuffle=True, num_workers=opts.n_cpu,
+        drop_last=True)  
+    val_loader = data.DataLoader(
+        val_dst, batch_size=opts.batch_size, shuffle=True, num_workers=opts.n_cpu)
+    print("Dataset: %s, Train set: %d, Val set: %d" %
+          (opts.dataset, len(train_dst), len(val_dst)))
+
+
+    model = eval(opts.model)(n_channels=3, backbone_name=opts.backbone)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=opts.lr, betas=(0.9, 0.999), 
+                                 eps=1e-08, weight_decay=opts.weight_decay)
+    metrics=SODMetrics(cuda=True)
+
+    opts.model+='_'+opts.backbone
+    if opts.lr_policy == 'poly':
+        scheduler = utils.PolyLR(optimizer, opts.total_itrs, power=0.9)
+    elif opts.lr_policy == 'step':
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=opts.step_size, gamma=0.1)
+
+    if opts.loss_type=='bce':
+        creterion=bce_DS5
+    elif opts.loss_type=='bsi':
+        creterion=bsi_DS5
+
+
+    def save_ckpt(path):
+        torch.save({
+            "epoch": epoch+1,
+            "model_state": model.state_dict(),
+            "optimizer_state": optimizer.state_dict(),
+            "scheduler_state": scheduler.state_dict(),
+        }, path)
+        print("Model saved as %s" % path)  
+        
+    cur_epoch=0
+    if opts.ckpt is not None and os.path.isfile(opts.ckpt):
+        checkpoint = torch.load(opts.ckpt, map_location=torch.device('cpu'))
+        model.load_state_dict(checkpoint["model_state"])
+        model = nn.DataParallel(model)
+        model=model.to(device)
+        
+        optimizer.load_state_dict(checkpoint["optimizer_state"])
+        scheduler.load_state_dict(checkpoint["scheduler_state"])
+        cur_epoch = checkpoint["epoch"]   
+        
+        print("Model restored from %s" % opts.ckpt)
+        del checkpoint  # free memory
+    else:
+        print("[!] Retrain")
+        model = nn.DataParallel(model)
+        model=model.to(device)
+
+
+    for epoch in range(cur_epoch,opts.epochs):
+        model.train()
+        cur_itrs=0
+        data_loader = tqdm(train_loader, file=sys.stdout)
+        running_loss = 0.0
+        
+        for (images, labels) in data_loader:
+            cur_itrs += 1
+
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.float32)
+
+            optimizer.zero_grad()
+           
+            S= model(images)
+            total_loss=creterion(S[0], S[1], S[2], S[3], S[4], labels)
+            
+            running_loss += total_loss.data.item()
+
+            total_loss.backward()
+            optimizer.step()
+
+            data_loader.desc = "Epoch {}/{}, loss={:.4f}".format(epoch, opts.epochs, running_loss/cur_itrs)
+            
+            scheduler.step()
+
+        if (epoch+1) % opts.val_interval == 0:
+            save_ckpt('CHKP/latest_{}_{}_{}.pth'.format(opts.model, 
+                                        opts.dataset, opts.loss_type))
+        
+        print("validation...")
+        model.eval()
+        val_score = validate(
+            opts=opts, model=model, loader=val_loader, device=device, metrics=metrics)
+        
+        print('val_score:',val_score)
+    
+        tags = ["train_loss", "learning_rate","MAE"]
+
+        tb_writer.add_scalar(tags[0], (running_loss/cur_itrs), epoch)
+        tb_writer.add_scalar(tags[1], optimizer.param_groups[0]["lr"], epoch)
+        tb_writer.add_scalar(tags[2], val_score["MAE"], epoch)
+
+
+if __name__ == '__main__':
+    main()
